c30166b872ec8c75bbee7a66544fb4194ce0bcd9
Ben Thorne <bn.thorne@gmail.com>
2023-05-01 15:20:03 -0700

    Added readme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="torch_planck2018_lite",
+    name="torch-planck2018-lite",
     version="0.1",
     packages=find_packages(),
     install_requires=[
@@ -9,7 +9,7 @@ setup(
         "torch",
     ],
     include_package_data=True,
-    package_data={"torch_planck2018_lite": ["data/*"]},
+    package_data={"torch-planck2018-lite": ["data/*"]},
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     test_suite="tests",
